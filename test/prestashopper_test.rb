f54a5f8b2c5b61c5e8b9ed69dea5ee10785b879d
Alfredo Amatriain <geralt@gmail.com>
2016-08-09 20:08:51 +0200

    Removed unused test.

--- test/prestashopper_test.rb
+++ test/prestashopper_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
 class PrestashopperTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Prestashopper::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+
 end
