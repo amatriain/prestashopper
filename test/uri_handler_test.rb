a323997179a62fbb048be100482ee504137a46cf
Alfredo Amatriain <geralt@gmail.com>
2016-08-11 13:23:37 +0200

    Added test.

--- test/uri_handler_test.rb
+++ test/uri_handler_test.rb
@@ -8,4 +8,11 @@ class UriHandlerTest < Minitest::Test
     api_uri = Prestashopper::UriHandler.api_uri base_uri
     assert_equal api_uri, expected_uri
   end
+
+  def test_get_api_uri_with_path
+    base_uri = 'http://my.prestashop.com/prestashop'
+    expected_uri = URI.parse 'http://my.prestashop.com/prestashop/api/'
+    api_uri = Prestashopper::UriHandler.api_uri base_uri
+    assert_equal api_uri, expected_uri
+  end
 end
