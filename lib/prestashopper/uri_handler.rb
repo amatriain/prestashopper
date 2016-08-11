d61ccd7d876cb09a45960e9b1f53d0d8758eab53
Alfredo Amatriain <geralt@gmail.com>
2016-08-11 18:38:45 +0200

    UriHandler returns String URIs, instead of URI class instances.

--- lib/prestashopper/uri_handler.rb
+++ lib/prestashopper/uri_handler.rb
@@ -8,7 +8,7 @@ module Prestashopper
     # @param base_url [String] a Prestashop base URL. Do not append "/api", it is appended internally. E.g. use
     #   "http://my.prestashop.com/", not "http://my.prestashop.com/api". URIs without scheme (e.g. "my.prestashop.com")
     #   are assumed to be http:// by default.
-    # @return [URI] the URI of the API for this Prestashop instance
+    # @return [String] the URI of the API for this Prestashop instance
     def self.api_uri(base_url)
       # Base URL must end in '/' so that we can properly append other path fragments for the API paths
       base_url.strip!
@@ -18,7 +18,7 @@ module Prestashopper
       base_url = 'http://' + base_url if URI.parse(base_url).scheme.nil?
 
       uri = URI.join base_url, API_PATH
-      return uri
+      return uri.to_s
     end
   end
 end
--- test/uri_handler_test.rb
+++ test/uri_handler_test.rb
@@ -4,21 +4,21 @@ class UriHandlerTest < Minitest::Test
 
   def test_get_api_uri
     base_uri = 'http://my.prestashop.com'
-    expected_uri = URI.parse 'http://my.prestashop.com/api/'
+    expected_uri = 'http://my.prestashop.com/api/'
     api_uri = Prestashopper::UriHandler.api_uri base_uri
     assert_equal api_uri, expected_uri
   end
 
   def test_get_api_uri_with_path
     base_uri = 'http://my.prestashop.com/prestashop'
-    expected_uri = URI.parse 'http://my.prestashop.com/prestashop/api/'
+    expected_uri = 'http://my.prestashop.com/prestashop/api/'
     api_uri = Prestashopper::UriHandler.api_uri base_uri
     assert_equal api_uri, expected_uri
   end
 
   def test_get_api_uri_without_scheme
     base_uri = 'my.prestashop.com'
-    expected_uri = URI.parse 'http://my.prestashop.com/api/'
+    expected_uri = 'http://my.prestashop.com/api/'
     api_uri = Prestashopper::UriHandler.api_uri base_uri
     assert_equal api_uri, expected_uri
   end
