ed8e863e0b835a68dc3f939d9be045aa3d26cdff
Alfredo Amatriain <geralt@gmail.com>
2016-08-12 12:59:15 +0200

    Check if an API key is valid.

--- lib/prestashopper.rb
+++ lib/prestashopper.rb
@@ -13,12 +13,31 @@ module Prestashopper
   #   E.g. use "http://my.prestashop.com", not "http://my.prestashop.com/api"
   # @param key [String] the key to check
   # @return [boolean] true if key is valid, false otherwise
+  # @raise [RestClient::Exception] if there is an error during HTTP GET
+  # @raise [StandardError] if a response different from 200 or 401 is received (not counting redirect responses,
+  #   which will be followed)
   def self.valid_key?(url, key)
-    return false
+    api_uri = UriHandler.api_uri url
+    res = RestClient::Resource.new api_uri, user: key, password: ''
+    begin
+      response = res.get
+      if response.code == 200 # OK response means API key is valid
+        return true
+      else
+        raise StandardError.new "Expected 200 or 401 response from Prestashop API #{api_uri} with key #{key}, instead received <#{response.code}> #{response.body}"
+      end
+    rescue RestClient::Exception => e
+      if e.response.code == 401
+        return false # "Unauthorized" response means API key is invalid
+      else
+        raise e # Any other HTTP error code means something is wrong
+      end
+    end
   end
 
   # Check if there is an enabled API in a Prestashop instance.
   # @param url [String] base URL of the Prestashop installation. Do not append "/api" to it, the gem does it internally.
+  #   E.g. use "http://my.prestashop.com", not "http://my.prestashop.com/api"
   # @return [boolean] true if the API is enabled, false if it is disabled
   # @raise [RestClient::Exception] if there is an error during HTTP GET
   # @raise [StandardError] if a response different from 401 or 503 is received (not counting redirect responses,
@@ -28,7 +47,8 @@ module Prestashopper
     res = RestClient::Resource.new api_uri, user: '', password: ''
     begin
       response = res.get
-      raise StandardError.new "Expected 401 or 503 response from Prestashop API, instead received <#{response.code}> #{response.body}"
+      # We don't send an API key, so an HTTP error code should be returned. Execution shouldn't reach here normally.
+      raise StandardError.new "Expected 401 or 503 response from Prestashop API #{api_uri} without key, instead received <#{response.code}> #{response.body}"
     rescue RestClient::Exception => e
       if e.response.code == 401
         return true # "Unauthorized" response means API is enabled
--- test/prestashopper_test.rb
+++ test/prestashopper_test.rb
@@ -5,7 +5,6 @@ class PrestashopperTest < Minitest::Test
   def setup
     @url = 'http://my.prestashop.com'
     @url_regex = %r{my[.]prestashop[.]com.*}
-    @web_stub = stub_request(:any, @url_regex)
   end
 
   def test_that_it_has_a_version_number
@@ -13,29 +12,31 @@ class PrestashopperTest < Minitest::Test
   end
 
   def test_checks_api_enabled
-    remove_request_stub @web_stub
     web_stub = stub_request(:any, @url_regex).to_return status: 503
     assert_equal Prestashopper.api_enabled?(@url), false
 
     remove_request_stub web_stub
-    web_stub = stub_request(:any, @url_regex).to_return status: 401
+    stub_request(:any, @url_regex).to_return status: 401
     assert_equal Prestashopper.api_enabled?(@url), true
   end
 
   def test_raises_error_checking_api_enabled
-    remove_request_stub @web_stub
-    web_stub = stub_request(:any, @url_regex).to_return status: 200
+    stub_request(:any, @url_regex).to_return status: 200
     assert_raises(StandardError) {Prestashopper.api_enabled? @url}
   end
 
-  def test_checks_invalid_key
-    key = 'invalid_key'
-    assert_equal Prestashopper.valid_key?(@url, key), false
+  def test_checks_valid_key
+    stub_request(:any, @url_regex).to_return status: 200
+    assert_equal Prestashopper.valid_key?(@url, 'valid_key'), true
   end
 
-  def test_checks_valid_key
-    skip
-    key = 'valid_key'
-    assert_equal Prestashopper.valid_key?(@url, key), true
+  def test_checks_invalid_key
+    stub_request(:any, @url_regex).to_return status: 401
+    assert_equal Prestashopper.valid_key?(@url, 'invalid_key'), false
+  end
+
+  def test_raises_error_checking_key_valid
+    stub_request(:any, @url_regex).to_return status: 500
+    assert_raises(StandardError) {Prestashopper.api_enabled? @url}
   end
 end
