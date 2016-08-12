7c76556aab55848776f0c23b79257e3a94695136
Alfredo Amatriain <geralt@gmail.com>
2016-08-12 13:36:40 +0200

    Reordered methods in the same order they will appear in docs.

--- lib/prestashopper.rb
+++ lib/prestashopper.rb
@@ -8,6 +8,31 @@ require 'prestashopper/uri_handler'
 # Ruby gem for interacting with the Prestashop API.
 module Prestashopper
 
+  # Check if there is an enabled API in a Prestashop instance.
+  # @param url [String] base URL of the Prestashop installation. Do not append "/api" to it, the gem does it internally.
+  #   E.g. use "http://my.prestashop.com", not "http://my.prestashop.com/api"
+  # @return [boolean] true if the API is enabled, false if it is disabled
+  # @raise [RestClient::Exception] if there is an error during HTTP GET
+  # @raise [StandardError] if a response different from 401 or 503 is received (not counting redirect responses,
+  #   which will be followed)
+  def self.api_enabled?(url)
+    api_uri = UriHandler.api_uri url
+    res = RestClient::Resource.new api_uri, user: '', password: ''
+    begin
+      response = res.get
+      # We don't send an API key, so an HTTP error code should be returned. Execution shouldn't reach here normally.
+      raise StandardError.new "Expected 401 or 503 response from Prestashop API #{api_uri} without key, instead received <#{response.code}> #{response.body}"
+    rescue RestClient::Exception => e
+      if e.response.code == 401
+        return true # "Unauthorized" response means API is enabled
+      elsif e.response.code == 503
+        return false # "Service Unavailable" response means API is disabled
+      else
+        raise e # Any other HTTP error code means something is wrong
+      end
+    end
+  end
+
   # Check if an API key is valid.
   # @param url [String] base URL of the Prestashop installation. Do not append "/api" to it, the gem does it internally.
   #   E.g. use "http://my.prestashop.com", not "http://my.prestashop.com/api"
@@ -35,28 +60,4 @@ module Prestashopper
     end
   end
 
-  # Check if there is an enabled API in a Prestashop instance.
-  # @param url [String] base URL of the Prestashop installation. Do not append "/api" to it, the gem does it internally.
-  #   E.g. use "http://my.prestashop.com", not "http://my.prestashop.com/api"
-  # @return [boolean] true if the API is enabled, false if it is disabled
-  # @raise [RestClient::Exception] if there is an error during HTTP GET
-  # @raise [StandardError] if a response different from 401 or 503 is received (not counting redirect responses,
-  #   which will be followed)
-  def self.api_enabled?(url)
-    api_uri = UriHandler.api_uri url
-    res = RestClient::Resource.new api_uri, user: '', password: ''
-    begin
-      response = res.get
-      # We don't send an API key, so an HTTP error code should be returned. Execution shouldn't reach here normally.
-      raise StandardError.new "Expected 401 or 503 response from Prestashop API #{api_uri} without key, instead received <#{response.code}> #{response.body}"
-    rescue RestClient::Exception => e
-      if e.response.code == 401
-        return true # "Unauthorized" response means API is enabled
-      elsif e.response.code == 503
-        return false # "Service Unavailable" response means API is disabled
-      else
-        raise e # Any other HTTP error code means something is wrong
-      end
-    end
-  end
 end
