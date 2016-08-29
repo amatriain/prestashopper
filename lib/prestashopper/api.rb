8c5891cf8c155b9aad31ff9c8196202405b53cb9
chaunce <chaunce.slc@gmail.com>
2016-08-29 15:11:49 -0600

    use method_missing to get resources

--- lib/prestashopper/api.rb
+++ lib/prestashopper/api.rb
@@ -50,5 +50,22 @@ module Prestashopper
 
       return products
     end
+
+    def method_missing(method, *args, &block)
+      if method.to_s.starts_with?('get_')
+        resource = method.to_s.sub(/^get_/,'').to_sym
+        raise 'You do not have access to this resource' unless resources.include?(resource)
+        if args.any?
+          ids = args.first.is_a?(Array) ? args.first : args
+          objects = ids.collect do |resource_id|
+            Hash.from_xml(Nokogiri::XML(@resources_res[resource][resource_id].get.body).remove_namespaces!.xpath("/prestashop/#{resource.to_s.singularize}").to_s).values
+          end.flatten.collect { |resource_hash| JSON.parse(resource_hash.to_json, object_class: OpenStruct) }
+        else
+          objects = Nokogiri::XML(@resources_res[resource].get.body).xpath("/prestashop/#{resource}/*/@id").collect(&:value)
+        end
+      else
+        super
+      end
+    end
   end
 end
