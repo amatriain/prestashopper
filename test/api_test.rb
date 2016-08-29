ec3ef5d3d6eabc63ebd77224a57ec0f71cf78450
chaunce <chaunce.slc@gmail.com>
2016-08-29 16:19:16 -0600

    update specs to match current code

--- test/api_test.rb
+++ test/api_test.rb
@@ -6,12 +6,12 @@ class ApiTest < Minitest::Test
     @url = 'http://my.prestashop.com'
     @url_regex = %r{my[.]prestashop[.]com.*}
     @key = 'VALID_KEY'
+
+    xml = File.open(File.join __dir__, 'xml', 'resources.xml').read
+    stub_request(:any, @url_regex).to_return body: xml
   end
 
   def test_get_resources
-    xml = File.open(File.join __dir__, 'xml', 'resources.xml').read
-    stub_request(:any, @url_regex).to_return body: xml
-
     resources = Prestashopper::API.new(@url, @key).resources
     assert_equal 3, resources.length
     [:customers, :orders, :products].each {|s| assert_includes resources, s}
@@ -21,15 +21,25 @@ class ApiTest < Minitest::Test
     xml_products = File.open(File.join __dir__, 'xml', 'products.xml').read
     stub_request(:any, %r{my[.]prestashop[.]com/api/products}).to_return body: xml_products
 
+    products_ids = Prestashopper::API.new(@url, @key).get_products
+    assert_equal 2, products_ids.length
+    ['1','2'].each {|id| assert_includes products_ids, id}
+  end
+
+  def test_get_products_records
+    xml_products = File.open(File.join __dir__, 'xml', 'products.xml').read
+    stub_request(:any, %r{my[.]prestashop[.]com/api/products}).to_return body: xml_products
+
     xml_product_1 = File.open(File.join __dir__, 'xml', 'product_1.xml').read
     stub_request(:any, %r{my[.]prestashop[.]com/api/products/1}).to_return body: xml_product_1
 
     xml_product_2 = File.open(File.join __dir__, 'xml', 'product_2.xml').read
     stub_request(:any, %r{my[.]prestashop[.]com/api/products/2}).to_return body: xml_product_2
 
-    products = Prestashopper::API.new(@url, @key).get_products
+    product_ids = Prestashopper::API.new(@url, @key).get_products
+    products = Prestashopper::API.new(@url, @key).get_products(product_ids)
     assert_equal 2, products.length
-    product_ids = products.map{|p| p['id']}
-    ['1','2'].each {|id| assert_includes product_ids, id}
+    products_ids = products.map{|p| p['id']}
+    ['1','2'].each {|id| assert_includes products_ids, id}
   end
 end
